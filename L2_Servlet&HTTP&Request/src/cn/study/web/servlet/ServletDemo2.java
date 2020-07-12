e1e5e97185b07eff0bc9bfd454cd249f1604ffee
keyboardWithDream <isharlan.hu@gmail.com>
2020-07-12 22:20:01 +0800

    Servlet体系结构+HttpServlet

---
+++ L2_Servlet&HTTP&Request/src/cn/study/web/servlet/ServletDemo2.java
@@ -0,0 +1,21 @@
+package cn.study.web.servlet;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+/**
+ * @author Harlan
+ * @date 2020/7/12 21:18
+ */
+@WebServlet("/demo2")
+public class ServletDemo2 extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Servlet继承了GenericServlet...");
+    }
+}
---
+++ L2_Servlet&HTTP&Request/src/cn/study/web/servlet/ServletDemo3.java
@@ -0,0 +1,29 @@
+package cn.study.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author Harlan
+ * @date 2020/7/12 22:01
+ */
+@WebServlet("/demo3")
+public class ServletDemo3 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("doGet方法执行...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("doPost方法执行...");
+    }
+}
