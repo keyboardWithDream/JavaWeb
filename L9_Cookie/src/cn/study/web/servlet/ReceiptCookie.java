b8ffb77b23c64e5092a91057b252ed69920a5689
keyboardWithDream <isharlan.hu@gmail.com>
2020-07-15 14:47:21 +0800

    Cookie会话技术

---
+++ L9_Cookie/src/cn/study/web/servlet/ReceiptCookie.java
@@ -0,0 +1,38 @@
+package cn.study.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author Harlan
+ * @date 2020/7/15 11:03
+ */
+@WebServlet("/receipt")
+public class ReceiptCookie extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        //获取Cookie
+        Cookie[] cookies = req.getCookies();
+        if(cookies != null){
+            for (Cookie cookie : cookies) {
+                String name = cookie.getName();
+                String value = cookie.getValue();
+                System.out.println(name+" <---> "+value);
+            }
+        }
+        System.out.println("======================换行信息=====================================");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+}
