c98c659dd18803d00c738e8ba13b7766f71c25c9
keyboardWithDream <isharlan.hu@gmail.com>
2020-07-16 14:26:24 +0800

    Session

---
+++ L12_Session/src/cn/study/web/servelt/SessionServlet.java
@@ -0,0 +1,32 @@
+package cn.study.web.servelt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * 使用Session共享数据
+ * @author Harlan
+ * @date 2020/7/16 12:17
+ */
+@WebServlet(urlPatterns = "/session")
+public class SessionServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.获取session
+        HttpSession session = req.getSession();
+        //2.存储数据
+        session.setAttribute("msg", "Hello Session!");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+}
