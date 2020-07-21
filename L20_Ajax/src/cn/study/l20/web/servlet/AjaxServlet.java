fcc6006266877d8c28648edf5dab3c737b23f183
keyboardWithDream <isharlan.hu@gmail.com>
2020-07-21 20:11:31 +0800

    Ajax + 原生JS实现

---
+++ L20_Ajax/src/cn/study/l20/web/servlet/AjaxServlet.java
@@ -0,0 +1,30 @@
+package cn.study.l20.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Harlan
+ * @date 2020/7/21 19:52
+ */
+@WebServlet(name = "/AjaxServlet", urlPatterns = ("/ajaxServlet"))
+public class AjaxServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String username = req.getParameter("username");
+        System.out.println(username);
+
+        resp.getWriter().write("Hello" + username);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+}
---
+++ L20_Ajax/web/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--
+  User: Harlan
+  Date: 2020/7/21
+  Time: 19:42
+--%>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
